8b259db19ab25eb409fdf26fb113c63be39b56fb
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 09:30:53 +0100

    Add files via upload

---
+++ ex07/src/index ex07.js
@@ -0,0 +1,15 @@
+function main () {
+    var StudLyCapVaR = 10;
+    var properCamelCase = "A String";
+    var TitleCaseOver = 9000;
+    STUDLYCAPVAR = 10;
+    PRoperCAmelCAse = "A String";
+    tITLEcASEoVER = 9000;
+    return {
+        studlyCapVar,
+        properCamelCase,
+        TitleCaseOver
+    }
+}
+console.log(main());
+module.exports=main;
