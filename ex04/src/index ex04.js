585b1cb3dc66ef1a1a426d1c36850b41e941d8c7
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 08:52:55 +0100

    Add files via upload

---
+++ ex04/src/index ex04.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    // Only change code above this line
+    return [x,y];
+}
+console.log(main())
+module.exports = main;
+myCat=10;
+ourCats=myCat;
+
