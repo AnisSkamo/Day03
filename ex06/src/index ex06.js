7349d833f9c0b39296bfb3196ca51cfaf74fcd18
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-11 09:11:18 +0100

    Add files via upload

---
+++ ex06/src/index ex06.js
@@ -0,0 +1,12 @@
+function main () {
+    var x = 5;
+    var y = 8;
+    var z = z + "My name is";
+    
+    x = x + 2;
+    y = y + 4;
+    z = z + "John";
+    return [x, y, z]
+}
+console.log(main());
+module.exports=main;
