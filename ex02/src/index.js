5c88c6920830b1b83c1b4b18679783c39e1e5773
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 12:16:10 +0100

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,14 @@
+function main(){
+    // Only change code below this line
+    // This is single line JavaScript comment
+    /*document write("This is a multi line comment!");
+    document write("This is a multi line comment!");
+    document write("This is a multi line comment!");
+    document write("This is a multi line comment!");
+    document write("This is a multi line comment!");
+    */
+    // Only change code above this line
 
+}
+console.log(main());
+module.esports = main;
