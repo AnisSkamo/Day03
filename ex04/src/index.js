bc8073cc292afb451a1ffd39e5d714d92648dd6d
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-12 12:32:03 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,11 @@
+function main() {
+    // Only change code below this line
+    x = 10;
+    y = 10;
+    x = y;
+    // Only change code above this line
+    return [x,y];
+}
 
+console.log(main());
+module.exports = main;
